9cf54521a3333d9d84475f5e255573556184a29d
Anthony <anthony-nek29@outlook.com>
2024-09-17 00:31:45 -0500

    Se actualizo el codigo agregando swagger

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,14 @@
 import { createCharacterHandler, listCharactersHandler } from './controllers/characterController';
-
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import * as swagger from './swagger/swagger.json';
 export const createCharacter = createCharacterHandler;
 export const listCharacters = listCharactersHandler;
+export const serveSwagger: APIGatewayProxyHandler = async (event) => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(swagger),
+    };
+  };
